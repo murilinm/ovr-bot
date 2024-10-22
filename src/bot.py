145ae4334343564fb70ee77fd2b6939e74c59a26
cubico <160647050+murilinm@users.noreply.github.com>
2024-10-22 18:49:04 +0000

    deleted:    src/__pycache__/bot.cpython-312.pyc 	deleted:    src/__pycache__/responses.cpython-312.pyc 	new file:   src/__pycache__/test.cpython-312.pyc 	modified:   src/bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,7 @@
 # IMPORTS
-import discord
-import discord.ext
-from discord.ext import commands
 from discord.ext.commands import Bot
+import discord
 from dpyConsole import Console
-import string
 
 # VARIABLES
 description = '''Oceanpoint Vacation Rentals bot commands, prefix "!"'''
@@ -14,6 +11,8 @@ intents.message_content = True
 bot = Bot(command_prefix='!', description=description, intents=intents)
 my_console = Console(discord.Client(intents=intents))
 my_id = 787065576995553301
+openedRentals = {}
+openedTickets = {}
 
 # - ID TEST SERVER VARIABLES
 test_guild_id = 1291429430895575080
@@ -34,6 +33,14 @@ async def on_ready():
 # SERVER COMMANDS
 @bot.tree.command(name='openrental', description='Open a rental channel')
 async def openrental(ctx: discord.Interaction, contact_info: str, people_in_house: int, renting_time: str, house_type: str, pets_in_house: bool):
+    times = 0
+    for i in range(1, len(openedRentals)):
+        if openedRentals[i] == ctx.user.id:
+            times += 1
+
+    if times >= 3:
+        return await ctx.response.send_message("You've reached the limit of rentals opened, please close a rental in order to open another one.")
+    
     name = ctx.user.name
     user = ctx.user
     guild = bot.get_guild(test_guild_id)
@@ -43,15 +50,14 @@ async def openrental(ctx: discord.Interaction, contact_info: str, people_in_hous
         user: discord.PermissionOverwrite(read_messages=True),
         guild.get_role(test_employee_role_id): discord.PermissionOverwrite(read_messages=True)
     }
-    channel = await category.create_text_channel(f'rental-{name[0]}{name[1]}{name[2]}{name[3]}', overwrites=overwrites)
+    
+    channel = await category.create_text_channel(f'rental-{name[:4]}', overwrites=overwrites)
     await ctx.response.send_message(f'Click [here](https://discord.com/channels/{guild.id}/{channel.id}) to go to your rental channel', ephemeral=True)
+    openedRentals[ctx.channel_id] = user.id
 
 @bot.group()
 async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
+    """Says if a user is cool."""
     if ctx.invoked_subcommand is None and ctx.message.author.id == my_id:
         await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
 
@@ -73,15 +79,16 @@ async def sync(ctx):
         await bot.tree.sync(guild=discord.Object(id=test_guild_id))
         await ctx.send(':white_check_mark:')
 
-# CONSOLE COMMANDS
-"""
+@bot.command()
+async def print(ctx, arg):
+    if ctx.message.author.id == my_id:
+        if arg == "openedRentals":
+            await ctx.send(openedRentals)
+
 @my_console.command()
 async def hey(user: discord.User):
-    print('hey')
-    await user.send('hey')
-"""
+    pass
 
-
-# STARTS CONSOLE AND BOT
+# STARTS BOT
 my_console.start()
 bot.run('MTI5MTQyODg5NzY5ODc0MjMyMw.GZD3Y5.Z-X9A6puKGPLV3gej1y1YwOtHhCqGRpZFw1wDY', reconnect=True)
