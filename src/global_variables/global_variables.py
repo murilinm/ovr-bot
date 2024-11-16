370b4f726f6507bbaa5bf034cf4cd115bb268238
cubico <160647050+murilinm@users.noreply.github.com>
2024-11-16 16:06:21 +0000

    modified:   src/bot.py 	modified:   src/global_variables/__pycache__/global_variables.cpython-312.pyc 	modified:   src/global_variables/global_variables.py 	modified:   src/ovr-bot.log 	new file:   src/static/transcripts/gen-muri-2/gen-muri-2.html

--- src/global_variables/global_variables.py
+++ src/global_variables/global_variables.py
@@ -1,16 +1,6 @@
 import json, logging, os
+logging.basicConfig(level=logging.INFO, filename="ovr-bot.log", format="%(asctime)s - %(levelname)s - %(message)s")
 
-# FUNCTIONS
-def log_to_file(fp, message):
-    log_file = os.path.join(os.path.dirname(__file__), '..', 'ovr-bot.log')
-    
-    if log_file:
-        with open(log_file, "a") as f:
-            logging.info(message)
-    else:
-        logging.error(f'logs/log.py function received invalid file path: {fp}')
-
-# CODE
 def update_json_file(file_path: str, new_data):
     with open(file_path, "r") as file:
         data = json.load(file)
@@ -21,8 +11,7 @@ def update_json_file(file_path: str, new_data):
         json.dump(data, file, indent=4, separators=(',', ': '))
     
     print(f"Updated {file_path} with: {new_data}")
-    #log.new_object(fp=file_path, new_data=new_data)
-    log_to_file(file_path, f"NEW OBJECT ON {file_path}: DATA {new_data}")
+    logging.info("NEW OBJECT ON {file_path}: DATA {new_data}")
 
 def delete_key(file_path: str, key_to_remove: str):
     with open(file_path, "r") as file:
@@ -35,9 +24,8 @@ def delete_key(file_path: str, key_to_remove: str):
         del data[key_to_remove]
         with open(file_path,'w') as f:
             json.dump(data,f,indent=4, separators=(',', ': '))
-            #log.object_deleted(file_path=file_path, key=key_to_remove, key_data=key_data)
 
-        log_to_file(file_path, f"OBJECT DELETED ON {file_path}: KEY {key_to_remove}; DATA: {key_data}")
+        logging.info(f"OBJECT DELETED ON {file_path}: KEY {key_to_remove}; DATA: {key_data}")
         return print(f"Removed {key_to_remove} ({key_data}) from {file_path}")
     else:
         return print(f"Error on deleting global variable key: {key_to_remove} from {file_path} does not exist.")
@@ -51,4 +39,4 @@ def update_specific_data(fp: str, new_data, key1: str, key2: str):
     with open(fp, "w") as f:
         json.dump(data, f, indent=4, separators=(',', ': '))
     print(f"Updated {key2} from {key1} ({fp}) with {new_data}")
-    log_to_file(fp, f"OBJECT UPDATED ON {fp}: KEY {key1}; OLD DATA: '{old}'; NEW DATA: {new_data}")
+    logging.info(f"OBJECT UPDATED ON {fp}: KEY {key1}; OLD DATA: '{key2}: {old}'; NEW DATA: '{key2}: {new_data}'")
